54fc88cef37c6b86ebab1afd88572f1ebfe87bb6
Cody Newman <ctman96@gmail.com>
2016-03-04 15:12:35 -0800

    Authors array in workspace

--- models/workspace.js
+++ models/workspace.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model('workspace',{
   title: String,
-  author:String,
+  author:[{String}],
   images:[{String}],
   panels:[{String}],
   width: Number,
