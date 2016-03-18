b3a68cf6840a06d125e35d842c3353e071ebc60d
Owen-Tyndall <owen.tyndall@gmail.com>
2016-03-18 15:16:22 -0700

    Added uploader field for panels

--- models/workspace.js
+++ models/workspace.js
@@ -3,11 +3,12 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model('workspace',{
   title: String,
-  author:[{String}],
-  images:[{String}],
-  panels:[{String}],
+  author:[String],
+  images:[String],
+  panels:[String],
   width: Number,
   height: Number,
+  uploaders:[String],
   tags:[String],
-  comic:[{String}]
+  comic:[String]
 });
