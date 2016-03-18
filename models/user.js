018ac9264ea5e564878b19ba3809ce5dda999730
Owen-Tyndall <owen.tyndall@gmail.com>
2016-03-18 15:14:37 -0700

    Added fields for Sprint 3 User stories
    does not include profile informtion user story

--- models/user.js
+++ models/user.js
@@ -4,4 +4,7 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('User',{
 	username: String,
 	password: String,
+	friends: [String],
+	messages: [{User:String,Message:String}],
+	favourites: [String],
 });
